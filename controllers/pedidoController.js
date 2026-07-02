8793826ec4e8306b4c4c9532e37901122384e4a5
Nozqy <bruno-romero@hotmail.com.ar>
2026-07-02 00:24:01 -0300

    Cambios en prodcutos, style.css, adicion de boton para generar pedido de articulos

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -9,3 +9,14 @@ exports.obtenerPedidos = async (req, res) => {
         res.status(500).json({ mensaje: 'Error al obtener los pedidos' });
     }
 };
+
+exports.crearPedido = async (req, res) => {
+    try {
+        const nuevoPedido = new Pedido(req.body);
+        await nuevoPedido.save();
+        res.json({ mensaje: 'Pedido guardado exitosamente' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensaje: 'Error al crear el pedido' });
+    }
+};
--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const pedidoController = require('../controllers/pedidoController');
 
 router.get('/', pedidoController.obtenerPedidos);
+router.post('/', pedidoController.crearPedido);
 
 module.exports = router;
